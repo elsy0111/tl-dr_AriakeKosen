111f52298246065f201d376b09c9e50f8f930087
elsy <76511273+elsy0111@users.noreply.github.com>
2023-10-12 04:09:28 +0900

    GUI =>Plan

--- CNN/py_gui.py
+++ CNN/py_gui.py
@@ -14,7 +14,27 @@ pygame.display.set_caption("Transparent Button Example")
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 font = pygame.font.SysFont("Serif", 50)
+fontL = pygame.font.SysFont("Serif", 70)
 
+init_Arr = [[0 for _ in range(H)] for _ in range(H)]
+f = open("./Plan/Move.txt", "w")
+f.write("[" + "\n")
+for ii in init_Arr:
+    f.write(str(ii) + ",\n")
+f.write("]")
+f.close()
+f = open("./Plan/Build.txt", "w")
+f.write("[" + "\n")
+for ii in init_Arr:
+    f.write(str(ii) + ",\n")
+f.write("]")
+f.close()
+f = open("./Plan/Break.txt", "w")
+f.write("[" + "\n")
+for ii in init_Arr:
+    f.write(str(ii) + ",\n")
+f.write("]")
+f.close()
 
 button_clear = (1000, 50,400,70)
 button_fp  =   ( 900,130,400,70)
@@ -25,6 +45,7 @@ button_ac  =   ( 900,450,400,70)
 button_ac1 =   (1000,530,400,70)
 button_ac2 =   (1000,610,400,70)
 button_ac3 =   (1000,690,400,70)
+button_Run =   (950,800,300,100)
 
 rect_clear = pygame.Rect(*button_clear)
 txt_clear = font.render("Clear", True, (0, 0, 0))
@@ -51,6 +72,10 @@ txt_ac2 = font.render("Build", True, (0, 0, 0))
 rect_ac3 = pygame.Rect(*button_ac3)
 txt_ac3 = font.render("Break", True, (0, 0, 0))
 
+rect_Run = pygame.Rect(*button_Run)
+txt_Run = fontL.render("Run", True, (0, 0, 0))
+
+
 #func ========================================================
 def click_field(p):
     return 0 <= p[0] < 900 and 0 <= p[1] < 900
@@ -76,7 +101,13 @@ def click_ac2(p):
 def click_ac3(p):
     return button_ac3[0] <= p[0] < button_ac3[0] + button_ac3[2] and button_ac3[1] <= p[1] < button_ac3[1] + button_ac3[3]
 
+def click_run(p):
+    return button_Run[0] <= p[0] < button_Run[0] + button_Run[2] and button_Run[1] <= p[1] < button_Run[1] + button_Run[3]
+
 Select_image = pygame.transform.scale(pygame.image.load(r"img\select.png").convert_alpha(), (dots, dots))
+Move_image = pygame.transform.scale(pygame.image.load(r"img\select_move.png").convert_alpha(), (dots, dots))
+Build_image = pygame.transform.scale(pygame.image.load(r"img\select_build.png").convert_alpha(), (dots, dots))
+Break_image = pygame.transform.scale(pygame.image.load(r"img\select_break.png").convert_alpha(), (dots, dots))
 choice_image = pygame.transform.scale(pygame.image.load(r"img\choice.png").convert_alpha(), (60, 60))
 
 past_mouse_position = (-1,-1)
@@ -128,6 +159,9 @@ while running:
     pygame.draw.rect(screen, (255, 0, 0), rect_ac3)
     screen.blit(txt_ac3, (button_ac3[0], button_ac3[1] + 5))
 
+    pygame.draw.rect(screen, (255, 0, 0), rect_Run)
+    screen.blit(txt_Run, (button_Run[0] + 90, button_Run[1] + 8))
+
     if Actions_pattern == 1:
         screen.blit(choice_image, (button_ac1[0] - 60, button_ac1[1] + 5))
     elif Actions_pattern == 2:
@@ -136,7 +170,12 @@ while running:
         screen.blit(choice_image, (button_ac3[0] - 60, button_ac3[1] + 5))
 
     for Rect_ in Selected_Rect:
-        screen.blit(Select_image, (Rect_[0] * dots,Rect_[1] * dots))
+        if Actions_pattern == 1:
+            screen.blit(Move_image, (Rect_[0] * dots,Rect_[1] * dots))
+        elif Actions_pattern == 2:
+            screen.blit(Build_image, (Rect_[0] * dots,Rect_[1] * dots))
+        elif Actions_pattern == 3:
+            screen.blit(Break_image, (Rect_[0] * dots,Rect_[1] * dots))
 
     for Rect_ in Selecting_Rect:
         screen.blit(Select_image, (Rect_[0] * dots,Rect_[1] * dots))
@@ -145,39 +184,69 @@ while running:
         mouse_position = pygame.mouse.get_pos()
 
         if click_clear(mouse_position):
-            Selected_Rect = []
-            Selecting_Rect = []
+            if mouse_position == past_mouse_position:
+                None
+            else:
+                past_mouse_position = mouse_position
+                Selected_Rect = []
+                Selecting_Rect = []
         elif click_fp0(mouse_position):
-            fill_pattern = 0
+            if mouse_position == past_mouse_position:
+                None
+            else:
+                past_mouse_position = mouse_position
+                fill_pattern = 0
         elif click_fp1(mouse_position):
-            fill_pattern = 1
+            if mouse_position == past_mouse_position:
+                None
+            else:
+                past_mouse_position = mouse_position
+                fill_pattern = 1
         elif click_fp2(mouse_position):
-            fill_pattern = 2
+            if mouse_position == past_mouse_position:
+                None
+            else:
+                past_mouse_position = mouse_position
+                fill_pattern = 2
         elif click_ac1(mouse_position):
-            Actions_pattern = 1
+            if mouse_position == past_mouse_position:
+                None
+            else:
+                past_mouse_position = mouse_position
+                Actions_pattern = 1
         elif click_ac2(mouse_position):
-            Actions_pattern = 2
+            if mouse_position == past_mouse_position:
+                None
+            else:
+                past_mouse_position = mouse_position
+                Actions_pattern = 2
         elif click_ac3(mouse_position):
-            Actions_pattern = 3
+            if mouse_position == past_mouse_position:
+                None
+            else:
+                past_mouse_position = mouse_position
+                Actions_pattern = 3
         elif click_field(mouse_position):
             if mouse_position == past_mouse_position:
                 None
             else:
                 past_mouse_position = mouse_position
                 p = (int(mouse_position[0]//dots), int(mouse_position[1]//dots))
-                if p in Selecting_Rect:
-                    Selecting_Rect.remove(p)
+                if fill_pattern == 0:
+                    if p in Selected_Rect:
+                        Selected_Rect.remove(p)
+                    else:
+                        Selected_Rect.append(p)
                 else:
-                    Selecting_Rect.append(p)
+                    if p in Selecting_Rect:
+                        Selecting_Rect.remove(p)
+                    else:
+                        Selecting_Rect.append(p)
                 if len(Selecting_Rect) == 2:
                     max_x = max(Selecting_Rect[0][0], Selecting_Rect[1][0])
                     min_x = min(Selecting_Rect[0][0], Selecting_Rect[1][0])
                     max_y = max(Selecting_Rect[0][1], Selecting_Rect[1][1])
                     min_y = min(Selecting_Rect[0][1], Selecting_Rect[1][1])
-                    if fill_pattern == 0:
-                        for k in Selecting_Rect:
-                            if not ((k[0], k[1]) in Selected_Rect):
-                                Selected_Rect.append((k[0], k[1]))
                     if fill_pattern == 1:
                         for i in range(min_y, max_y + 1):
                             for j in range(min_x, max_x + 1):
@@ -191,10 +260,52 @@ while running:
                                         Selected_Rect.append((j, i))
                     Selecting_Rect = []
                 # print(mouse_position)
-                print(p[0], p[1])
+                print("Pressed", p[0], p[1])
+        elif click_run(mouse_position):
+            if mouse_position == past_mouse_position:
+                None
+            else:
+                past_mouse_position = mouse_position
+                Selecting_Rect = []
+                if Selected_Rect == []:
+                    print("Non-Selected")
+                else:
+                    print(Selected_Rect)
+                    if Actions_pattern == 1:
+                        f = open("./Plan/Move.txt", "r")
+                        Arr = eval(f.read().replace("\n",""))
+                        f.close()
+                        f = open("./Plan/Move.txt", "w")
+                    if Actions_pattern == 2:
+                        f = open("./Plan/Build.txt", "r")
+                        Arr = eval(f.read().replace("\n",""))
+                        f.close()
+                        f = open("./Plan/Build.txt", "w")
+                    if Actions_pattern == 3:
+                        f = open("./Plan/Break.txt", "r")
+                        Arr = eval(f.read().replace("\n",""))
+                        f.close()
+                        f = open("./Plan/Break.txt", "w")
+                    
+                    for r in Selected_Rect:
+                        Arr[r[1]][r[0]] = 1
+                    f.write("[" + "\n")
+                    for ii in Arr:
+                        f.write(str(ii) + ",\n")
+                    f.write("]")
+                    f.close()
+                    
+                    Selected_Rect = []
         else:
             None
 
     pygame.display.flip()
 
 pygame.quit()
+
+# How to Read Plan File
+"""
+    f = open("./Plan/Break.txt", "r")
+    Arr = eval(f.read().replace("\n",""))
+    f.close()
+"""
